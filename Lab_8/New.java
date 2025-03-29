c2bae231a9a76084bc5a10b8cccb7f571f66c195
AbdulrahmanSlaghour <abduslaghour@gmail.com>
2025-03-29 22:13:46 +0300

    Create New.java

---
+++ Lab_8/New.java
@@ -0,0 +1,16 @@
+// Removed package declaration to match the directory structure
+
+
+public class New extends Customer {
+    //-------------------------------------
+    public New()
+    {
+        
+    }
+    //--------------------------------------
+    public New(String name,double purchasesCost)
+    {
+        super(name,purchasesCost);
+    }
+    
+}
