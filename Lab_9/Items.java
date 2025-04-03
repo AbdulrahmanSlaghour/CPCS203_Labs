9f3ea897aa2303246c61e5b3bbe8ba6122f6a7c1
AbdulrahmanSlaghour <abduslaghour@gmail.com>
2025-04-03 07:22:37 +0300

    Create Items.java

---
+++ Lab_9/Items.java
@@ -0,0 +1,48 @@
+public class Items {
+    //***PROPERTIES***
+    private String itemName;
+    private double unitPrice;
+    private int quantity;
+    
+    //***CONSTRUCTORS***
+    public Items() {
+    }
+    
+    public Items(String itemName, double unitPrice, int quantity) {
+        this.itemName = itemName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+    
+    
+    
+    //***METHODS***
+    
+    public String getItemName() {
+        return getItemName;
+    }
+    
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    
+    public double getUnitPrice() {
+        return this.unitPrice;
+    }
+    
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    
+    public int getQuantity() {
+        return this.quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public String toString() {
+        return "Item Properties: [itemName= " + this.itemName + ", unitPrice= " + this.unitPrice + ", quantity= " + this.quantity;
+    }
+}
