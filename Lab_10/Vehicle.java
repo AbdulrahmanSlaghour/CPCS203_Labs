2e13eb041ea2e35fde31f53d4442891b575b5ebb
AbdulrahmanSlaghour <abduslaghour@gmail.com>
2025-04-09 22:18:28 +0300

    Update Vehicle.java
    Changed spelling of "italicised" to "italicized" to match correct spelling

--- Lab_10/Vehicle.java
+++ Lab_10/Vehicle.java
@@ -4,7 +4,7 @@ public interface Vehicle {
 
     //***BEHAVIOURS***
 
-    //all behaviours have been italicised, meaning they are meant to be abstract,
+    //all behaviours have been italicized, meaning they are meant to be abstract,
     //but since all interface methods are abstract by default there is no need to
     //add the abstract keyword
     public int getNumberOfSeats();
